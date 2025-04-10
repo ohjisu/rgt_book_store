562cfc3d82d44cd70716aceb80aab22aaf03f55d
ohjisu <oh.suroc@gmail.com>
2025-04-10 19:42:31 +0900

    cors 추가

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.books import router as book_router
 from app.db.init_db import init_models
 from app.db.session import engine
@@ -7,6 +8,14 @@ from app.db.session import engine
 app = FastAPI()
 app.include_router(book_router)
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def on_startup_event():
--- app/models/book.py
+++ app/models/book.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, DateTime, String, Boolean
+from sqlalchemy import Column, Integer, DateTime, String, Boolean, UniqueConstraint
 from datetime import datetime
 from app.models.base import Base
 
@@ -8,3 +8,7 @@ class Book(Base):
     author = Column(String(255), nullable=False)
     sales = Column(Integer, default=0, nullable=False)
     is_deleted = Column(Boolean, default=False, nullable=False)
+
+    __table_args__ = (
+        UniqueConstraint('title','author', name='uiq_title_author'),
+    )
